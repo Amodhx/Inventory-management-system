c25c62d523663b56730b455d847fd331dac9baff
Amodhx <amxdhnanditha@gmail.com>
2024-05-11 10:21:49 +0530

    inventory js done

--- controller/customer.js
+++ controller/customer.js
@@ -1,3 +1,110 @@
 import CustomerModel from "../model/customerModel.js";
 import {customers} from "../db/db.js";
 
+
+let index;
+$("#customerSavebtn").on('click', () => {
+    let btnText = $("#customerSavebtn").text();
+    if (btnText == "Update") {
+        customers[index].customerId = $("#customerIdField").val();
+        customers[index].customerName = $("#cusrtomerNameField").val();
+        customers[index].customerAddress = $("#customerAddressField").val();
+        customers[index].customerContact = $("#customerContactField").val();
+
+        loadCustomerValues();
+        let modelel = $("#modelAddCustomer");
+        var modal = bootstrap.Modal.getInstance(modelel)
+        modal.hide();
+    } else {
+
+        var regexContact = /^\d{10}$/;
+        if (regexContact.test($("#customerContactField").val())) {
+            let cusId = $("#customerIdField").val();
+            let cusContact = $("#customerContactField").val();
+            let cusName = $("#cusrtomerNameField").val();
+            let cusAddress = $("#customerAddressField").val();
+            let date = Date.now();
+
+            let customer = new CustomerModel(cusId, cusName, cusAddress, cusContact, date);
+            customers.push(customer);
+            loadCustomerValues();
+            let modelel = $("#modelAddCustomer");
+            var modal = bootstrap.Modal.getInstance(modelel)
+            modal.hide();
+        }else {
+            alert("Wrong Contact Number!!");
+        }
+    }
+});
+
+
+function loadCustomerValues() {
+    $("#customerTbody").empty();
+
+    customers.map(function (customer) {
+        var value =
+            ` <tr>
+                <td class="customerId-val">${customer.customerId}</td>
+                <td class="customerName-val">${customer.customerName}</td>
+                <td class="customerAddress-val">${customer.customerAddress}</td>
+                <td class="customerContact-val">${customer.customerContact}</td>
+                <td class="customerDate-val">${customer.customerAddDate}</td>
+            </tr>`
+
+        $("#customerTbody").append(value);
+    });
+}
+
+// table row click
+$("#customerTbody").on('click', 'tr', function () {
+    console.log("AMOdh")
+    let id = $(this).find('.customerId-val').text();
+    let name = $(this).find('.customerName-val').text();
+    let address = $(this).find('.customerAddress-val').text();
+    let contact = $(this).find('.customerContact-val').text();
+    index = $(this).index();
+
+    $('#modelAddCustomer').modal('show');
+
+    $("#customerSavebtn").text("Update");
+    $("#customerModelHeader").text("Update Customer");
+
+    $("#customerDeleteBtn").text("Delete");
+    $("#customerDeleteBtn").css({
+        background : "red"
+    })
+    $("#customerIdField").val(id);
+    $("#customerContactField").val(contact);
+    $("#cusrtomerNameField").val(name);
+    $("#customerAddressField").val(address);
+
+});
+
+$("#customerDeleteBtn").on('click',function () {
+    let btnText = $("#customerDeleteBtn").text();
+    if (btnText == "Delete") {
+        customers.splice(index, 1);
+        loadCustomerValues();
+        let modelel = $("#modelAddCustomer");
+        var modal = bootstrap.Modal.getInstance(modelel)
+        modal.hide()
+    }else {
+        let modelel = $("#modelAddCustomer");
+        var modal = bootstrap.Modal.getInstance(modelel)
+        modal.hide()
+    }
+})
+
+$("#btn").on('click', function () {
+    $('#modelAddCustomer').modal('show');
+    $("#customerSavebtn").text("Save Changes");
+    $("#customerModelHeader").text("Add Customer");
+    $("#customerIdField").val("");
+    $("#customerContactField").val("");
+    $("#cusrtomerNameField").val("");
+    $("#customerAddressField").val("");
+    $("#customerDeleteBtn").text("Close");
+    $("#customerDeleteBtn").css({
+        background : "gray"
+    })
+})
--- controller/inventory.js
+++ controller/inventory.js
@@ -1,10 +1,28 @@
 import InventoryModel from "../model/inventoryModel.js";
-import {inventories} from "../db/db.js";
+import {customers, inventories} from "../db/db.js";
 
 
 
 // inventory save item on btn click
 $("#inventorySavebtn").on('click',()=>{
+
+    let text = $("#inventorySavebtn").text();
+    if (text == "Update"){
+        console.log("Update")
+        inventories[index].productName = $("#inputProduct").val();
+        inventories[index].buyingPrice = $("#inputbuyPrice").val();
+        inventories[index].sellingPrice = $("#inputSelprice").val();
+        inventories[index].qty = $("#inputQTY").val();
+        inventories[index].brand = $("#inputBrand").val();
+        inventories[index].expireDate = $("#inputEXDate").val();
+
+        loadInventoryTable();
+        let modelel = $("#exampleModalInventory");
+        var modal = bootstrap.Modal.getInstance(modelel)
+        modal.hide();
+
+    }else {
+        console.log("save")
         let productName = $("#inputProduct").val();
         let buyPrice = $("#inputbuyPrice").val();
         let exDate = $("#inputEXDate").val();
@@ -12,14 +30,16 @@ $("#inventorySavebtn").on('click',()=>{
         let qty = $("#inputQTY").val();
         let brand = $("#inputBrand").val();
 
-        let item = new InventoryModel(productName,buyPrice,selPrice,qty,brand,exDate);
+        let item = new InventoryModel(productName, buyPrice, selPrice, qty, brand, exDate);
         inventories.push(item);
         loadInventoryTable();
 
+        clearFileds();
 
         let modelel = $("#exampleModalInventory");
         var modal = bootstrap.Modal.getInstance(modelel)
         modal.hide();
+    }
 });
 
 
@@ -31,17 +51,99 @@ function loadInventoryTable(){
     inventories.map(function (item) {
         var value =
            ` <tr>
-                <td>${item.productName}</td>
-                <td>${item.buyingPrice}</td>
-                <td>${item.qty}</td>
-                <td>${item.brand}</td>
-                <td>${item.sellingPrice}</td>
-                <td>${item.expireDate}</td>
+                <td class="itemName-val" >${item.productName}</td>
+                <td class="itemBuyPrice-val">${item.buyingPrice}</td>
+                <td class="itemQty-val">${item.qty}</td>
+                <td class="itemBrand-val">${item.brand}</td>
+                <td class="itemSelPrice-val">${item.sellingPrice}</td>
+                <td class="itemExDate-val">${item.expireDate}</td>
                 <td>"In Stock"</td>
-                <td><input type="button" value="Update"></td>
-                <td><input type="button" value="Delete"></td>
             </tr>`
 
         $("#inventoryTbody").append(value);
     });
 }
+function clearFileds() {
+    $("#inputProduct").val("");
+    $("#inputbuyPrice").val("");
+    $("#inputEXDate").val("");
+    $("#inputSelprice").val("");
+    $("#inputQTY").val("");
+    $("#inputBrand").val("");
+}
+
+let index;
+
+
+
+// table row click
+$("#inventoryTbody").on('click','tr',function () {
+    let iname = $(this).find('.itemName-val').text();
+    let ibuyPrice = $(this).find('.itemBuyPrice-val').text();
+    let iqty = $(this).find('.itemQty-val').text();
+    let ibrand = $(this).find('.itemBrand-val').text();
+    let iselPrice = $(this).find('.itemSelPrice-val').text();
+    let iexDate = $(this).find('.itemExDate-val').text();
+
+    index = $(this).index();
+    console.log( iexDate )
+
+    $("#inputProduct").val(iname);
+    $("#inputbuyPrice").val(ibuyPrice);
+    $("#inputEXDate").val(iexDate);
+    $("#inputSelprice").val(iselPrice);
+    $("#inputQTY").val(iqty);
+    $("#inputBrand").val(ibrand);
+
+
+    $("#exampleModalInventory").modal('show');
+
+
+    $("#itemModelClose").text("Delete")
+    $("#itemModelClose").css({
+        background:"red"
+    })
+
+    $("#ItemModelHeader").text("Update Item")
+    $("#inventorySavebtn").text("Update")
+
+
+
+});
+
+
+
+
+// item delete or model close
+$("#itemModelClose").on('click',function (){
+    let btnText = $("#itemModelClose").text();
+    if (btnText == "Delete") {
+        console.log("delete")
+        inventories.splice(index, 1);
+        loadInventoryTable();
+        let modelel = $("#exampleModalInventory");
+        var modal = bootstrap.Modal.getInstance(modelel)
+        modal.hide()
+    }else {
+        console.log("close")
+        let modelel = $("#exampleModalInventory");
+        var modal = bootstrap.Modal.getInstance(modelel)
+        modal.hide()
+    }
+});
+
+
+
+// btn addItem on click
+$("#btnAddItem").on('click',function () {
+
+    $("#exampleModalInventory").modal('show');
+    $("#itemModelClose").text("Close")
+    $("#itemModelClose").css({
+        background:"gray"
+    })
+
+    $("#ItemModelHeader").text("Add Item")
+    $("#inventorySavebtn").text("Save Changes")
+
+})
